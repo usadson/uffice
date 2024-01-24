c65267e2ebc7f1367d4d86b853a3627ed93cd182
Tristan Gerritsen <code@thewoosh.org>
2024-01-24 18:18:06 +0100

    feat: Start port macOS

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,11 +1,21 @@
 // Copyright (C) 2022 Tristan Gerritsen <tristan@thewoosh.org>
 // All Rights Reserved.
 
+#[cfg(target_os = "macos")]
+pub mod macos;
+
 #[cfg(windows)]
 pub mod windows;
 
+#[cfg(target_os = "macos")]
+pub use self::macos as implementation;
+
 #[cfg(windows)]
 pub use self::windows as implementation;
+
+pub fn show_message_box_blocking(title: &str, message: &str) {
+    implementation::show_message_box_blocking(title, message);
+}
 
 pub fn open_file_user(path: &str) {
     implementation::open_file_user(path);
